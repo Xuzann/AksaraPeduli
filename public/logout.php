cfc4d934e79999028fc5fb31e91599abeb7a9999
Xuzann <23082010143@student.upnjatim.ac.id>
2025-05-14 23:04:04 +0700

    integrating laravel

--- public/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start(); // Mulai session
-session_unset();  // Hapus semua variabel sesi
-session_destroy(); // Hancurkan sesi
-setcookie(session_name(), '', time() - 3600, '/'); // Hapus cookie sesi
-
-echo json_encode(["status" => "success"]);
-exit;
